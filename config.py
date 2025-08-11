fc1dfe6129ee0e8604a9e9ba3a562847ba2d59fd
HAN SHEN <145312662+jaydentan040622@users.noreply.github.com>
2025-08-11 09:07:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "database-1.cp2qesi406qm.us-east-1.rds.amazonaws.com"
+customhost = "employee.c71zo5vqvdro.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Aacs3253"
+custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "tanhanshen-employee"
 customregion = "us-east-1"
-
